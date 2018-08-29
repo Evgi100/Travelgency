48028df102cc44b4c3901af926eb76876f401c9a
Evgi100 <evgeni.shapira@gmail.com>
2018-08-29 13:59:57 +0300

    Add animations using waypoint

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+
 import mobileMenu from './modules/mobileMenu';
+import revealOnScroll from './modules/revealOnScroll';
 
 var mobilemenu = new mobileMenu();
+new revealOnScroll($('.feature-item'), '85%');
+new revealOnScroll($('.testimonial'), '60%');
